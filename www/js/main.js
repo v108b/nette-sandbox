248b3092775611d3d6b1e42ba9272140ca0187e5
Martin Matoušek <martin.matousek@androsoft.cz>
2015-03-14 16:23:27 +0100

    bootstrap, bsforms

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,20 @@
-$(function(){
+﻿$(function() {
+    $('.slim-scroll').slimScroll({});
+
+    $('.date-picker').datepicker({
+        autoclose: true,
+        format: 'dd.mm.yyyy',
+        language: 'cs',
+    });
+
+    $('.date-picker-input').datepicker({
+        autoclose: true,
+        format: 'dd.mm.yyyy',
+        language: 'cs',
+    });
+
+    $('textarea:not(.noresize)').autosize({
+        append: false
+    });
 
 });
