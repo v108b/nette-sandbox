8e3fd1abcf3a318833b7c1c7c451016b673037cc
Martin Matoušek <martin.matousek@androsoft.cz>
2015-03-14 21:53:04 +0100

    crud extension, but how it wokrs?

---
+++ app/presenters/CrudPresenter.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Presenters;
+class CrudPresenter extends BasePresenter {
+	public function actionListTable($tableName)
+	{
+		$this['crudList'] = $this->getCrudExtenstion()->getComponents()->buildTableGrid($tableName);
+	}
+	/**
+	 * @return \V108B\NetteCrud\Extension
+	 */
+	public function getCrudExtenstion()
+	{
+		return $this->context->getService('crud');
+	}
+}
